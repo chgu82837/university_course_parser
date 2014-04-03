2381edaf310263551deaaaa0a62854bf809753e0
C. J. Hwong <CJHwong@gmail.com>
2014-04-04 07:21:25 +0800

    Add time property to NTNU

--- ntnu/ntnu.py
+++ ntnu/ntnu.py
@@ -1,6 +1,32 @@
 import json
 from xlrd import open_workbook
 
+def normalize(raw_time):
+    if raw_time == '':
+        return ''
+
+    times = raw_time.split(',')
+
+    days = {'一': '1', '二': '2', '三': '3', '四': '4',
+            '五': '5', '六': '6', '七': '7'}
+
+    to_word = {1: '1', 2: '2', 3: '3', 4: '4', 5: 'N',
+               6: '5', 7: '6', 8: '7', 9: '8', 10: '9',
+               11: 'A', 12: 'B', 13: 'C', 14: 'D', 15: 'E'}
+
+    for time in times:
+        if time == '':
+            continue
+        time = time.split(' ')
+        result = days[time[0]]
+        raw = time[1].split('-')
+        start = int(raw[0])
+        stop = int(raw[1]) if len(raw) > 1 else int(raw[0])
+        for i in range(start, stop+1):
+            result += to_word[i+1]
+
+    return result
+
 def to_json(subject):
     items = ['serial', 'code', 'department', 'team', 'grade',
              'class', 'language', 'moocs', 'sex', 'title',
@@ -10,7 +36,11 @@ def to_json(subject):
 
     data = {}
     for i in range(len(subject)):
-        data[items[i]] = subject[i]
+        data[items[i]] = subject[i].strip()
+
+    # Normalize particular data
+    data['obligatory'] = '必修' if data['obligatory'] == '必' else '選修'
+    data['time'] = normalize(data['location'])
 
     # Remove useless data
     useless = ['team', 'moocs', 'sex', 'eng_title',
@@ -30,7 +60,7 @@ def open_excel():
     table = excel.sheets()[0]
 
     nrows = table.nrows
-    for i in range(nrows):
+    for i in range(1, nrows):
         subject = []
         colnames = table.row_values(i)
         for col in colnames:
